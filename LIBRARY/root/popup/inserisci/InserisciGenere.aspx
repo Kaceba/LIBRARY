7c35184fb9f8ff78e60b1a2d0ac66243f60a22c9
Roberto Ninno <RobertoNinno23@outlook.it>
2022-07-26 11:49:22 +0200

    added gestione generi

---
+++ LIBRARY/root/popup/inserisci/InserisciGenere.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="InserisciGenere.aspx.cs" Inherits="root_popup_inserisci_InserisciTipoSpesa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Genere:"></asp:Label>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtGenere" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="btnInvia" runat="server" Text="Invia" OnClick="btnInvia_Click"  />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ LIBRERIACLASSI/GENERI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class GENERI
+{
+    public GENERI()
+    {
+
+    }
+
+    public class AGGIORNADB
+    {
+        public int CodiceGenere;
+        public string DescrizioneGenere;
+
+        //insert
+        public void Insert() 
+        {
+            CONNESSIONE C = new CONNESSIONE();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "GENERI_INSERT";
+            cmd.Parameters.AddWithValue("@DescrizioneGenere", DescrizioneGenere);
+
+            C.EseguiSPcmdparam(cmd);
+        }
+
+        //update
+        public void Update()
+        {
+            CONNESSIONE C = new CONNESSIONE();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "GENERI_UPDATE";
+            cmd.Parameters.AddWithValue("CodiceGenere", CodiceGenere);
+            cmd.Parameters.AddWithValue("DescrizioneGenere", DescrizioneGenere);
+
+            C.EseguiSPcmdparam(cmd);
+        }
+
+    }
+
+    //selectall
+    public DataTable Select()
+    {
+        CONNESSIONE C = new CONNESSIONE();
+
+        return C.EseguiSp("GENERI_SELECTALL");
+    }
+
+    //selectone
+    public DataTable Select(int CodiceGenere)
+    {
+        CONNESSIONE c = new CONNESSIONE();
+        SqlCommand cmd = new SqlCommand();
+
+        cmd.CommandText = "GENERI_SELECTONE";
+        cmd.Parameters.AddWithValue("@CodiceGenere", CodiceGenere);
+
+        return c.EseguiSPselectparam(cmd);
+    }
+
+    //checkone
+    public bool CheckOne(string DescrizioneGenere)
+    {
+        CONNESSIONE c = new CONNESSIONE();
+        DataTable dt = new DataTable();
+        SqlCommand cmd = new SqlCommand();
+
+        cmd.CommandText = "GENERI_CHECKONE";
+        cmd.Parameters.AddWithValue("@DescrizioneGenere", DescrizioneGenere);
+
+        dt = c.EseguiSPselectparam(cmd);
+
+        return dt.Rows.Count > 0;
+    }
+}
+
